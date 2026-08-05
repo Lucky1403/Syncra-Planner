d1dd5e94c64c09b403ebc1122ca0d67544aeca19
Lucky1403 <lucky.230062289@gmail.com>
2026-08-05 10:41:22 +0530

    Changed the Hashing limit from 128 to 255

--- api/index.py
+++ api/index.py
@@ -105,7 +105,7 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     events = db.relationship('Event', backref='user', lazy=True, cascade="all, delete-orphan")
 
     def set_password(self, password):
